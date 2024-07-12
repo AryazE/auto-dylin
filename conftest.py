2f3afc571952622fa95adbd484974e7efeb3c50e
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-07-12 11:53:03 +0200

    New approach

--- conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_sessionstart(session):
-    print("sessionstart")
-    print(session)
-    print(dir(session))
-
-def pytest_sessionfinish(session, exitstatus):
-    print("sessionfinish")
-    print(session, exitstatus)
